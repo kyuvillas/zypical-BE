4406061a417fbb9545e494c78e354ef0b082b829
JK Go <jkvgo08@gmail.com>
2017-07-02 23:10:59 -0700

    homepage ready. Working on Login Modal

--- public/angular/Modules/Home/homeController.js
+++ public/angular/Modules/Home/homeController.js
@@ -1,153 +1,180 @@
-app.controller('HomeController', function ($location, $document) {
-    var h = this;
-    
-//    h.SVG = ConfigurableItems.SVG;
-//
-//    h.ostat = 2;
-//    h.ChangeOstat = function (id) {
-//        h.ostat = id;
-//    }
+(function(){
+    'use strict';
+    angular.module('home')
+        .controller('HomeController', HomeController);
+                    
+        function HomeController($location, $document, $mdDialog){
+            var h = this;
 
-    h.goToArea = function(area){
-        var chosenElement = angular.element(document.getElementById(area));
-        $document.scrollToElementAnimated(chosenElement, 0, 1000);
-//        $location.hash(area);
-//        $anchorScroll();
-    }
+        //    h.SVG = ConfigurableItems.SVG;
+        //
+        //    h.ostat = 2;
+        //    h.ChangeOstat = function (id) {
+        //        h.ostat = id;
+        //    }
 
-//    h.openMenu = function($mdMenu, ev) {
-//      // originatorEv = ev;
-//      $mdMenu.open(ev);
-//    };
-//    
-//    h.showDialog = showDialog;
-//    h.items = [1, 2, 3];
-//
-//    h.try= function(){
-//      DataFactory.SignUp().
-//            then(function(resonse) {
-//                alert(resonse.data);
-//            })
-//    }
-//    function showDialog($event,loc) {
-//       var parentEl = angular.element(document.body);
-//       $mdDialog.show({
-//         parent: parentEl,
-//         targetEvent: $event,
-//         templateUrl:'angular/Modules/Home/login-dialog.html',
-//         locals: {
-//           items: h.items,
-//           loc:loc
-//         },
-//         controller: DialogController,
-//         clickOutsideToClose: true,
-//      escapeToClose: true
-//      });
-//      function DialogController(h, $mdDialog, items,loc) {
-//        h.items = items;
-//        h.selectedTab = loc;
-//        console.log(loc);
-//        h.closeDialog = function() {
-//          $mdDialog.hide();
-//        }
-//
-//        h.submit = function() {
-//          // you can check is form valid with help of $valid parameter
-//          if(h.formName.$valid){
-//              // alert("Name is " + h.username);
-//              var res = $rootScope.SignIn(h.username,h.password);
-//                
-//                
-//                if(res == 'Unauthorized'){
-//                  alert('Unauthorized credentials');
-//                }else if(res.role == 'seller'){
-//                  console.log(res);
-//                  $state.go('storeprofile');
-//                }
-//
-//            
-//          }
-//        }
-//
-//        h.signup = function() {
-//          // you can check is form valid with help of $valid parameter
-//          if(h.signupForm.$valid){
-//                var user = {
-//                  "fname": h.signup_fname,
-//                  "lname": h.signup_lname,
-//                  "gender": h.signup_gender,
-//                  "username": h.signup_username,
-//                  "password": h.signup_pass,
-//                  "emailadd": h.signup_emailadd
-//                }
-//                DataFactory.SignUp(user)
-//                .then(function (response) {
-//                   console.log(response.data);
-//                   if(response.data){
-//                      // $mdDialog.hide();
-//                      h.login(user);
-//
-//                   }
-//                });             
-//          }else{ 
-//            angular.forEach(h.signupForm.$error.required, function(field) {
-//                field.$setTouched();
-//            });
-//          }
-//        }
-//
-//
-//        h.credentials = {};
-//        h.loginForm = {};
-//        h.error = false;
-//        
-//        //when the form is submitted
-//        h.login_submit = function() {
-//          h.submitted = true;
-//          if (!h.loginForm.$invalid) {
-//            h.login(h.login);
-//          } else {
-//            angular.forEach(h.loginForm.$error.required, function(field) {
-//                field.$setTouched();
-//            });
-//            h.error = true;
-//            return;
-//          }
-//        };
-//
-//        //Performs the login function, by sending a request to the server with the Auth service
-//        h.login = function(credentials) {
-//          h.error = false;
-//          Auth.login(credentials, function(user) {
-//            //success function
-//            $mdDialog.hide();
-//            $state.go('userprofile');
-//          }, function(err) {
-//            console.log("error");
-//            h.error = true;
-//          });
-//        };
-//        
-//        // if a session exists for current user (page was refreshed)
-//        // log him in again
-//        
-//
-//        h.logout = function(){
-//          Auth.login();
-//        }
-//      }
-//    }
-//    h.login =function(credentials) {
-//      h.error = false;
-//      Auth.login(credentials, function(user) {
-//        //success function
-//        $mdDialog.hide();
-//        $state.go('userprofile');
-//      }, function(err) {
-//        console.log("error");
-//        h.error = true;
-//      });
-//    };
-    
+            h.goToArea = function(area){
+                var chosenElement = angular.element(document.getElementById(area));
+                $document.scrollToElementAnimated(chosenElement, 0, 1000);
+        //        $location.hash(area);
+        //        $anchorScroll();
+            }
+            
+            h.showLogInDialog = function(ev){
+                $mdDialog.show({
+                    controller: LoginController,
+                    templateUrl: '../public/angular/Modules/Home/login.tmpl.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: true,
+                    fullscreen: false
+                });
+            }
+            
+            function LoginController($scope, $mdDialog){
+                $scope.hide = function(){
+                    $mdDialog.hide();
+                };
+                
+                $scope.cancel = function(){
+                    $mdDialog.cancel();
+                };
+            }
 
-})
+        //    h.openMenu = function($mdMenu, ev) {
+        //      // originatorEv = ev;
+        //      $mdMenu.open(ev);
+        //    };
+        //    
+        //    h.showDialog = showDialog;
+        //    h.items = [1, 2, 3];
+        //
+        //    h.try= function(){
+        //      DataFactory.SignUp().
+        //            then(function(resonse) {
+        //                alert(resonse.data);
+        //            })
+        //    }
+        //    function showDialog($event,loc) {
+        //       var parentEl = angular.element(document.body);
+        //       $mdDialog.show({
+        //         parent: parentEl,
+        //         targetEvent: $event,
+        //         templateUrl:'angular/Modules/Home/login-dialog.html',
+        //         locals: {
+        //           items: h.items,
+        //           loc:loc
+        //         },
+        //         controller: DialogController,
+        //         clickOutsideToClose: true,
+        //      escapeToClose: true
+        //      });
+        //      function DialogController(h, $mdDialog, items,loc) {
+        //        h.items = items;
+        //        h.selectedTab = loc;
+        //        console.log(loc);
+        //        h.closeDialog = function() {
+        //          $mdDialog.hide();
+        //        }
+        //
+        //        h.submit = function() {
+        //          // you can check is form valid with help of $valid parameter
+        //          if(h.formName.$valid){
+        //              // alert("Name is " + h.username);
+        //              var res = $rootScope.SignIn(h.username,h.password);
+        //                
+        //                
+        //                if(res == 'Unauthorized'){
+        //                  alert('Unauthorized credentials');
+        //                }else if(res.role == 'seller'){
+        //                  console.log(res);
+        //                  $state.go('storeprofile');
+        //                }
+        //
+        //            
+        //          }
+        //        }
+        //
+        //        h.signup = function() {
+        //          // you can check is form valid with help of $valid parameter
+        //          if(h.signupForm.$valid){
+        //                var user = {
+        //                  "fname": h.signup_fname,
+        //                  "lname": h.signup_lname,
+        //                  "gender": h.signup_gender,
+        //                  "username": h.signup_username,
+        //                  "password": h.signup_pass,
+        //                  "emailadd": h.signup_emailadd
+        //                }
+        //                DataFactory.SignUp(user)
+        //                .then(function (response) {
+        //                   console.log(response.data);
+        //                   if(response.data){
+        //                      // $mdDialog.hide();
+        //                      h.login(user);
+        //
+        //                   }
+        //                });             
+        //          }else{ 
+        //            angular.forEach(h.signupForm.$error.required, function(field) {
+        //                field.$setTouched();
+        //            });
+        //          }
+        //        }
+        //
+        //
+        //        h.credentials = {};
+        //        h.loginForm = {};
+        //        h.error = false;
+        //        
+        //        //when the form is submitted
+        //        h.login_submit = function() {
+        //          h.submitted = true;
+        //          if (!h.loginForm.$invalid) {
+        //            h.login(h.login);
+        //          } else {
+        //            angular.forEach(h.loginForm.$error.required, function(field) {
+        //                field.$setTouched();
+        //            });
+        //            h.error = true;
+        //            return;
+        //          }
+        //        };
+        //
+        //        //Performs the login function, by sending a request to the server with the Auth service
+        //        h.login = function(credentials) {
+        //          h.error = false;
+        //          Auth.login(credentials, function(user) {
+        //            //success function
+        //            $mdDialog.hide();
+        //            $state.go('userprofile');
+        //          }, function(err) {
+        //            console.log("error");
+        //            h.error = true;
+        //          });
+        //        };
+        //        
+        //        // if a session exists for current user (page was refreshed)
+        //        // log him in again
+        //        
+        //
+        //        h.logout = function(){
+        //          Auth.login();
+        //        }
+        //      }
+        //    }
+        //    h.login =function(credentials) {
+        //      h.error = false;
+        //      Auth.login(credentials, function(user) {
+        //        //success function
+        //        $mdDialog.hide();
+        //        $state.go('userprofile');
+        //      }, function(err) {
+        //        console.log("error");
+        //        h.error = true;
+        //      });
+        //    };
+
+
+        }
+})();
--- public/angular/main.js
+++ public/angular/main.js
@@ -1,72 +1,81 @@
-/* MAIN MODULE */
-var app = angular.module('MainApplication', ['ngSanitize','zy-Auth','zy-Router', 'ngAnimate', 'ngMaterial', 'jQueryScrollbar', 'HUD-ContentManager', 'HUD-Factory', 'HUD-Directives', 'ang-drag-drop', 'duScroll']); //
-app.controller('MainController', function ($window,$rootScope,$scope,$state, $interval, $mdDialog, ConfigurableItems, DataFactory, $mdSidenav,Auth, AUTH_EVENTS,USER_ROLES) {
-  // SEARCH
-  $scope.search = function(key){
-      console.log(key);
-      $state.go('search1',{keyword:key});
-    
-    
-  }
-    $scope.mainstate = $state;
-    $scope.ChangeState = function (state) {
-        $state.go(state);
-    }
+(function(){
+    'use strict';
+        /* MAIN MODULE */
+    angular
+        .module('MainApplication', ['ngSanitize','zy-Auth','zy-Router', 'ngAnimate', 'ngMaterial', 'jQueryScrollbar', 'HUD-ContentManager', 'HUD-Factory', 'HUD-Directives', 'ang-drag-drop', 'duScroll', 'home'])
+        .config(['$mdThemingProvider', function($mdThemingProvider){
+            $mdThemingProvider.generateThemesOnDemand(true);
+        }])
+        .controller('MainController', MainController);
+                    
+        function MainController($window,$rootScope,$scope,$state, $interval, $mdDialog, ConfigurableItems, DataFactory, $mdSidenav,Auth, AUTH_EVENTS,USER_ROLES) {
+            
+              // SEARCH
+              $scope.search = function(key){
+                  console.log(key);
+                  $state.go('search1',{keyword:key});
+              }
+                $scope.mainstate = $state;
+                $scope.ChangeState = function (state) {
+                    $state.go(state);
+                }
 
-    if ($window.localStorage["userInfo"]) {
-      var credentials = JSON.parse($window.localStorage["userInfo"]);
-        login(credentials);
-    }
-  function login(credentials) {
-          $scope.error = false;
-          Auth.login(credentials, function(user) {
-            //success function
-            // $mdDialog.hide();
-            if($scope.mainstate.current.name=='home')  $state.go('userprofile');
-          }, function(err) {
-            $scope.error = true;
-          });
+                if ($window.localStorage["userInfo"]) {
+                  var credentials = JSON.parse($window.localStorage["userInfo"]);
+                    login(credentials);
+                }
+              function login(credentials) {
+                      $scope.error = false;
+                      Auth.login(credentials, function(user) {
+                        //success function
+                        // $mdDialog.hide();
+                        if($scope.mainstate.current.name=='home')  $state.go('userprofile');
+                      }, function(err) {
+                        $scope.error = true;
+                      });
+                    };
+                $scope.modalShown = false;
+                $scope.auth = 'notAuthenticated';
+                var showLoginDialog = function() {
+                //do something when not authorized
+                    $scope.auth = 'notAuthenticated';
+                };
+              $scope.logout = function() {
+                //do something when not authorized
+                Auth.logout();
+                $state.go('home');
+              };
+
+                var setCurrentUser = function(){
+                    $scope.auth = 'loginSuccess';
+                $scope.isLoggedIn = 'true';
+                    $scope.currentUser = $rootScope.currentUser;	
+                }
+                var setGuestUser = function(){
+                $scope.auth = 'guestSuccess';
+                $scope.isLoggedIn = 'true';
+                $scope.currentUser = $rootScope.currentUser;  
+              }
+                var showNotAuthorized = function(){
+                    $scope.auth = 'notAuthorized';
+                    // alert("Not Authorized");
+                }
+
+                $scope.currentUser = null;
+                $scope.userRoles = USER_ROLES;
+                $scope.isAuthorized = Auth.isAuthorized;
+
+                //listen to events of unsuccessful logins, to run the login dialog
+                $rootScope.$on(AUTH_EVENTS.notAuthorized, showNotAuthorized);
+                $rootScope.$on(AUTH_EVENTS.notAuthenticated, showLoginDialog);
+                $rootScope.$on(AUTH_EVENTS.sessionTimeout, showLoginDialog);
+                $rootScope.$on(AUTH_EVENTS.logoutSuccess, showLoginDialog);
+                $rootScope.$on(AUTH_EVENTS.loginSuccess, setCurrentUser);
+              $rootScope.$on(AUTH_EVENTS.guestSuccess, setGuestUser);
+
+
+
         };
-	$scope.modalShown = false;
-	$scope.auth = 'notAuthenticated';
-	var showLoginDialog = function() {
-    //do something when not authorized
-		$scope.auth = 'notAuthenticated';
-	};
-  $scope.logout = function() {
-    //do something when not authorized
-    Auth.logout();
-    $state.go('home');
-  };
-	
-	var setCurrentUser = function(){
-		$scope.auth = 'loginSuccess';
-    $scope.isLoggedIn = 'true';
-		$scope.currentUser = $rootScope.currentUser;	
-	}
-	var setGuestUser = function(){
-    $scope.auth = 'guestSuccess';
-    $scope.isLoggedIn = 'true';
-    $scope.currentUser = $rootScope.currentUser;  
-  }
-	var showNotAuthorized = function(){
-		$scope.auth = 'notAuthorized';
-		// alert("Not Authorized");
-	}
-	
-	$scope.currentUser = null;
-	$scope.userRoles = USER_ROLES;
-	$scope.isAuthorized = Auth.isAuthorized;
+})();
 
-	//listen to events of unsuccessful logins, to run the login dialog
-	$rootScope.$on(AUTH_EVENTS.notAuthorized, showNotAuthorized);
-	$rootScope.$on(AUTH_EVENTS.notAuthenticated, showLoginDialog);
-	$rootScope.$on(AUTH_EVENTS.sessionTimeout, showLoginDialog);
-	$rootScope.$on(AUTH_EVENTS.logoutSuccess, showLoginDialog);
-	$rootScope.$on(AUTH_EVENTS.loginSuccess, setCurrentUser);
-  $rootScope.$on(AUTH_EVENTS.guestSuccess, setGuestUser);
-
-
-
-})
 
